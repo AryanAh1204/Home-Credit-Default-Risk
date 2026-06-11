135fef721865ea4c9c6ad1f6214cd0d49ec552e5
Aryan Ahlawat <aryanahlawat1204@gmail.com>
2026-06-11 16:53:54 +0530

    Create 04_default_rate_income_type_x_payment_burden.sql

---
+++ sql/04_default_rate_income_type_x_payment_burden.sql
@@ -0,0 +1,14 @@
+select
+	t.name_income_type,
+  	CASE
+    	WHEN amt_annuity / amt_income_total < 0.1 THEN 'Low burden (<10%)'
+    	WHEN amt_annuity / amt_income_total < 0.2 THEN 'Moderate (10-20%)'
+    	WHEN amt_annuity / amt_income_total < 0.3 THEN 'High (20-30%)'
+    ELSE 'Severe (>30%)'
+  	END AS payment_burden_bucket,
+  	COUNT(*) AS total_applicants,
+  	ROUND(AVG(target)*100, 2) AS default_rate
+FROM application_train t
+WHERE amt_income_total > 0
+GROUP BY 1 , 2 
+ORDER BY 4 DESC;
